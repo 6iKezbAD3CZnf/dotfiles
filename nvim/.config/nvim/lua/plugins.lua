a38a4354d09dc209b2d57ac07d19b615b4f3c75f
Tatsuya Kubo <6iKezbAD3CZnf@gmail.com>
2024-04-08 20:15:37 +0900

    Update nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,38 +2,37 @@ require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    use 'williamboman/mason.nvim'    
+    use 'williamboman/mason.nvim'
     use 'williamboman/mason-lspconfig.nvim'
 
-    use 'neovim/nvim-lspconfig' 
-    use 'simrat39/rust-tools.nvim'
+    -- use 'neovim/nvim-lspconfig'
+    -- use 'simrat39/rust-tools.nvim'
 
     -- Completion framework:
-    use 'hrsh7th/nvim-cmp' 
+    use 'hrsh7th/nvim-cmp'
 
     -- LSP completion source:
-    use 'hrsh7th/cmp-nvim-lsp'
+    -- use 'hrsh7th/cmp-nvim-lsp'
 
     -- Useful completion sources:
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
-    use 'hrsh7th/cmp-path'                              
-    use 'hrsh7th/cmp-buffer'                            
-    use 'hrsh7th/vim-vsnip'
+    -- use 'hrsh7th/cmp-nvim-lua'
+    -- use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    -- use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
+    -- use 'hrsh7th/vim-vsnip'
 
-    use 'puremourning/vimspector'
+    -- use 'puremourning/vimspector'
 
     use 'voldikss/vim-floaterm'
 
-    use 'sainnhe/everforest'
     use 'rebelot/kanagawa.nvim'
 
-    use 'nvim-lua/plenary.nvim'
-    use({
-      "nvim-telescope/telescope.nvim",
-      requires = { { "nvim-lua/plenary.nvim" }, { "nvim-lua/popup.nvim" } },
-    })
+    -- use 'nvim-lua/plenary.nvim'
+    -- use({
+    --   "nvim-telescope/telescope.nvim",
+    --   requires = { { "nvim-lua/plenary.nvim" }, { "nvim-lua/popup.nvim" } },
+    -- })
 
     use {
       'nvim-lualine/lualine.nvim',
@@ -42,17 +41,47 @@ require('packer').startup(function(use)
 
     use {'stevearc/dressing.nvim'}
 
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        requires = { 
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-        }
-    }
+    -- use {
+    --     "nvim-neo-tree/neo-tree.nvim",
+    --     branch = "v2.x",
+    --     requires = {
+    --         "nvim-lua/plenary.nvim",
+    --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    --         "MunifTanjim/nui.nvim",
+    --     }
+    -- }
 
-    use 'lervag/vimtex'
+    -- use 'lervag/vimtex'
+
+    use 'h-hg/fcitx.nvim'
+
+    use 'github/copilot.vim'
+
+    -- install without yarn or npm
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+    -- use({
+    --     "iamcco/markdown-preview.nvim",
+    --     run = "cd app && npm install",
+    --     setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    --     ft = { "markdown" },
+    -- })
+
+    -- use 'bilalq/lite-dfm'
+
+    use 'junegunn/goyo.vim'
+
+    -- use 'mikewest/vimroom'
+
+    -- use 'jose-elias-alvarez/null-ls.nvim'
+
+    -- use 'jose-elias-alvarez/typescript.nvim'
+
+    -- ALE
+    use 'dense-analysis/ale'
 end)
 
 -- Mason Setup
@@ -68,26 +97,26 @@ require("mason").setup({
 require("mason-lspconfig").setup()
 
 -- Rust Tools
-local rt = require("rust-tools")
+-- local rt = require("rust-tools")
 
-rt.setup({
-  tools = { -- rust-tools options
-    executor = require("rust-tools.executors").termopen,
-    runnables = {
-      use_telescope = true
-    },
-  },
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
+-- rt.setup({
+--   tools = { -- rust-tools options
+--     executor = require("rust-tools.executors").termopen,
+--     runnables = {
+--       use_telescope = true
+--     },
+--   },
+--   server = {
+--     on_attach = function(_, bufnr)
+--       -- Hover actions
+--       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+--       -- Code action groups
+--       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--     end,
+--   },
+-- })
 
--- LSP Diagnostics Options Setup 
+-- LSP Diagnostics Options Setup
 local sign = function(opts)
   vim.fn.sign_define(opts.name, {
     texthl = opts.name,
@@ -124,12 +153,12 @@ vim.diagnostic.config({
 -- updatetime: set updatetime for CursorHold
 vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
 vim.opt.shortmess = vim.opt.shortmess + { c = true}
-vim.api.nvim_set_option('updatetime', 300) 
+vim.api.nvim_set_option('updatetime', 300)
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error 
--- Show inlay_hints more frequently 
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
 vim.cmd([[
 set signcolumn=no
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
@@ -166,7 +195,7 @@ cmp.setup({
     { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
     { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
     { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip
     { name = 'calc'},                               -- source for math calculation
   },
   window = {
@@ -226,7 +255,23 @@ vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Next()', { silent = true, script 
 vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Previous()', { silent = true, script = true, expr = true })
 
 -- lualine
-require('lualine').setup()
+-- require('lualine').setup()
 
 -- neotree
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+-- goyo
+vim.g.goyo_width = 150
+vim.g.goyo_height = 49
+vim.keymap.set('n', "<leader>g", ":Goyo<CR>")
+
+-- -- null-ls
+-- local null_ls = require("null-ls")
+-- null_ls.setup({
+--     sources = {
+--         null_ls.builtins.formatting.eslint,
+--     },
+-- })
+
+-- ALE
+vim.g.ale_completion_enabled = 0
